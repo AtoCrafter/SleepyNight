d3bad257f8de24220dbe6c2416553bfad393769d
AtoCrafter <ato.crafter@gmail.com>
2014-11-28 12:11:42 +0900

    MOD 情報のアノテーションを 1.7.10 用に更新

--- src/main/java/ato/sleepynight/SleepyNight.java
+++ src/main/java/ato/sleepynight/SleepyNight.java
@@ -1,25 +1,11 @@
 package ato.sleepynight;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(
-        modid = "SleepyNight",
-        name = "Sleepy Night",
-        version = "@VERSION@"
-)
-@NetworkMod(
-        clientSideRequired = true,
-        serverSideRequired = false
-)
+@Mod(modid = "SleepyNight")
 public class SleepyNight {
-
-    @Instance("SleepyNight")
-    public static SleepyNight instance;
     @SidedProxy(
             serverSide = "ato.sleepynight.CommonProxy",
             clientSide = "ato.sleepynight.ClientProxy"
